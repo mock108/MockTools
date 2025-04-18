d665ba56e443a16600905cd13788f787d822c009
k-ura <k-ura@192.168.1.51>
2025-04-18 16:55:41 +0900

    AlgolTool追加 Git関連修正

---
+++ AlgolTool/src/main/java/io/github/mockup/mocktoolsparent/AlgolToolApplication.java
@@ -0,0 +1,13 @@
+package io.github.mockup.mocktoolsparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgolToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgolToolApplication.class, args);
+	}
+
+}
